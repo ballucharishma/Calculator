aef46d9b31f2907518756634e67d99ab3f975e68
istar <istar@192.168.100.2>
2017-05-25 17:57:41 +0530

    localvariable..by charishma

---
+++ src/com/onlinecalculator/calculator/calctypes/localvariable.java
@@ -0,0 +1,19 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+public class localvariable {
+	static void display(){
+		int x=10;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("local variable x="+x);
+		x=sc.nextInt();
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		localvariable s=new localvariable();
+	  display();
+
+		
+	}
+
+}
