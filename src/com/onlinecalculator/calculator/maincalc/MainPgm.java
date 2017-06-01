4655effeefe2ec4b48b77bcd0b3e2228eaafa926
HOME <HOME@192.168.0.21>
2017-05-31 19:47:06 -0700

    Calculator Main Program Code-B.Charishma

--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -6,26 +6,43 @@ import com.onlinecalculator.calculator.calctypes.*;
 
 public class MainPgm {
 public static void main(String args[]){
-        char type;
-        type=getType();
-        if(type=='a')
+        int type;
+        type=disp();
+       
+        if(type==1)
         {
+        	BasicCalc  cal=new BasicCalc();
+        	cal.menu();
+        	cal.userInputs();
+        	cal.performOperation();
         	
         }
-        if(type=='b')
+        if(type==2)
         {
-       		
-       	}
-        	
-        	if(type=='c')
-        	{
-        		
-        	}
- 
-    }
+        	ScientificCalc  cal=new ScientificCalc();
+        	cal.menu();
+        	cal.userInputs();
+        	cal.performOperation();
+        }
         
-    static char getType()
-    {
-    	
-    }
+        if(type==3)
+        {
+        	StatisticCalc  cal=new StatisticCalc();
+        	cal.menu();
+        	cal.userInputs();
+        	cal.performOperation();
+        }
+}
+   static int disp(){
+	   int ch;
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("***************Calculator Program**********************");
+	   System.out.println("***************1. Basic Calculator Program**********************");
+	   System.out.println("***************2. Scientific Calculator Program**********************");
+	   System.out.println("***************3. Satistical Calculator Program**********************");
+	   System.out.println("*****************************************************");
+	   System.out.println("Enter your Choice");
+	   ch=sc.nextInt();
+	   return(ch);
+   }
 }
